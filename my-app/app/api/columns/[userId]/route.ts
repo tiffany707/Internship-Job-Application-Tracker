ed9272dd77e38fb4c71ae944fcbdd8074c1ec3da
tiffany707 <tiffanyokura7@gmail.com>
2026-02-24 19:20:56 -0700

    fix: Changed userId number > string for columns

--- my-app/app/api/columns/[userId]/route.ts
+++ my-app/app/api/columns/[userId]/route.ts
@@ -23,7 +23,7 @@ export async function GET(req:Request, {params}:{params:Promise<{userId:string}>
 }
 
 
-export async function PATCH(req:Request, {params}:{params:{userId:string}}){
+export async function PATCH(req:Request, {params}:{params:Promise<{userId:string}>}){
     try{
         const paramsData = await params
         const data = await req.json();
